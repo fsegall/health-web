ff38d315630253fe0473569f7ea95e453c8438b4
fsegall <segall.felipe@gmail.com>
2020-10-18 17:00:54 -0300

    Starts styles for household list items

--- src/pages/Household/index.tsx
+++ src/pages/Household/index.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation, Link } from "react-router-dom";
-import { FiChevronLeft } from 'react-icons/fi';
+import { FiChevronLeft, FiHome } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
 import ICreateHouseholdDTO from '../../pages/Interview/dtos/ICreateHouseholdDTO';
 import ICreatePersonDTO from '../../pages/Interview/dtos/ICreatePersonDTO';
-import { HouseList, Header, DefaultContent } from './styles';
+import { HouseholdContainer, HouseList, Header, DefaultContent } from './styles';
 
 /* const { token } = useAuth(); */
 /* const [house, setHouse] = useState<ICreateHousehold | null>(null); */
@@ -55,21 +55,37 @@ const Household: React.FC = () => {
         <h1>Safety <span>|</span> Residência</h1>
       </Header>
       { household ?
-        (<HouseList>
-          <li>
-            <div>
+        (<HouseholdContainer>
+          <div><FiHome size="40" style={{ margin: 10 }} color="#ff9000" /></div>
+          <HouseList>
+
+            <li>
+
+
               <span>Região de Localização da Casa: </span>
               {household?.location_of_residence}
-            </div>
-          </li>
-          <li>
-            <div>
+
+            </li>
+            <li>
+
+              <span>{`Tipo de residência: ${household?.type_of_residence}`}</span>
+
+            </li>
+            <li>
+
               <span>{`A pessoa ${household?.household_main_person ? 'é ' : 'não é '
                 } a figura de referência da casa`}</span>
-            </div>
-          </li>
-          <li></li>
-        </HouseList >) : <DefaultContent>Esta pessoa ainda não tem uma residência associada a ela na pesquisa</DefaultContent>}
+
+            </li>
+            <li>
+
+              <span>{`A pessoa  ${household?.government_assistance_program_bolsa_familia ? 'recebeu ' : 'não recebeu '
+                } assitência do bolsa família`}</span>
+
+            </li>
+
+          </HouseList >
+        </HouseholdContainer>) : <DefaultContent>Esta pessoa ainda não tem uma residência associada a ela na pesquisa</DefaultContent>}
     </>
 
   )
--- src/pages/Household/styles.ts
+++ src/pages/Household/styles.ts
@@ -1,16 +1,29 @@
 import styled from 'styled-components';
 
+export const HouseholdContainer = styled.div`
+display: grid;
+grid-template-columns: 1fr 3fr;
+`;
+
 export const DefaultContent = styled.div`
   color: #59748c;
   padding: 20px;
 `;
 
 export const HouseList = styled.ul`
+
+  padding-top: 10px;
+  list-style-type: none;
   color: #59748c;
   line-height: 30px;
 
   font-weight: bold;
   font-size: 18px;
+  li {
+    border-bottom: 1px solid #ccc;
+    margin-bottom: 10px;
+    span {margin-left: 10px;}
+    }
 `;
 
 export const Header = styled.div`
