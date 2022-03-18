ab8d3dc39d33e3ccd0dad79bcdce8b2bf19118b3
fsegall <segall.felipe@gmail.com>
2022-03-18 17:32:05 -0300

    Edits select options

--- src/pages/Interview/questions/SelectorOptions/options.ts
+++ src/pages/Interview/questions/SelectorOptions/options.ts
@@ -365,7 +365,7 @@ export const auxilio_vezes = [
   { value: 'uma vez', label: 'Uma vez' },
   { value: 'duas vezes', label: 'Duas vezes' },
   { value: 'três vezes', label: 'Três vezes' },
-  { value: 'três vezes ou mais', label: 'Três vezes ou mais' },
+  { value: 'três vezes', label: 'Três vezes' },
   { value: 'ns-nr', label: 'NS/NR' },
 ];
 
