f8be519ee2d403a23a90e287794d335c2d7c8a77
fsegall <segall.felipe@gmail.com>
2022-04-13 00:38:16 -0300

    Edit

--- src/pages/OfflineInterviews/index.tsx
+++ src/pages/OfflineInterviews/index.tsx
@@ -1,23 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
+import api from '../../services/api';
+import { useAuth } from '../../hooks/auth';
 import ICreateOfflineInterviewDTO from '../Interview/dtos/ICreateOfflineInterviewDTO';
 
-
 import {
-
+    OfflineButton,
+    Container
 } from './styles';
-
-
 
 
 const OfflineInterviews: React.FC = () => {
 
-/*     const interviews: { [key: string]: ICreateOfflineInterviewDTO } = JSON.parse(localStorage.getItem('@Safety:offline-interviews') || '{}'); */
-const interviews: string = localStorage.getItem('@Safety:offline-interviews') || '';
+    const { token } = useAuth();
 
-    return (<div>
+    const interviewsObject: { [key: string]: ICreateOfflineInterviewDTO } = JSON.parse(localStorage.getItem('@Safety:offline-interviews') || '{}');
+    const interviews: string = localStorage.getItem('@Safety:offline-interviews') || '';
+
+    async function OnClick() {
+        await api.post('/offline', {interviews}, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+    }
+
+    return (<Container>
                 <h1>Interviews</h1>
-                <div>{interviews}</div>
-            </div>
+                <OfflineButton onClick={OnClick}>Send</OfflineButton>
+                {/* <div>{interviews}</div> */}
+            </Container>
             );
 
 };
