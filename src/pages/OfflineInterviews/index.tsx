91fc65e2c72ddfe147a73a1c14dbd6e7e0cc9715
fsegall <segall.felipe@gmail.com>
2022-04-13 00:47:24 -0300

    Fix for build

--- src/pages/OfflineInterviews/index.tsx
+++ src/pages/OfflineInterviews/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import api from '../../services/api';
 import { useAuth } from '../../hooks/auth';
+
 /* import ICreateOfflineInterviewDTO from '../Interview/dtos/ICreateOfflineInterviewDTO'; */
 
 import {
