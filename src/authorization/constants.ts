c66ed7766c1ad059206169878f1f6ca59d6eaa23
Amanda Manduca <amandavmanduca@hotmail.com>
2022-10-18 13:19:50 -0300

    :passport_control: Setting VISITOR role to all functionalities permissions

--- src/authorization/constants.ts
+++ src/authorization/constants.ts
@@ -17,12 +17,12 @@ const Actions = {
 
 const mappings = new Map();
 
-mappings.set(Actions.VIEW_ALL_INTERVIEWS, [Roles.COORDINATOR, Roles.ADMIN]);
-mappings.set(Actions.VIEW_MY_INTERVIEWS, [Roles.INTERVIEWER, Roles.COORDINATOR, Roles.ADMIN]);
-mappings.set(Actions.VIEW_PROJECTS, [Roles.COORDINATOR, Roles.ADMIN]);
-mappings.set(Actions.CREATE_PROJECT, [Roles.COORDINATOR, Roles.ADMIN]);
-mappings.set(Actions.CREATE_INTERVIEW, [Roles.INTERVIEWER, Roles.COORDINATOR, Roles.ADMIN]);
-mappings.set(Actions.ASSIGN_INTERVIEWER_ROLE, [Roles.COORDINATOR, Roles.ADMIN]);
+mappings.set(Actions.VIEW_ALL_INTERVIEWS, [Roles.COORDINATOR, Roles.ADMIN, Roles.VISITOR]);
+mappings.set(Actions.VIEW_MY_INTERVIEWS, [Roles.INTERVIEWER, Roles.COORDINATOR, Roles.ADMIN, Roles.VISITOR]);
+mappings.set(Actions.VIEW_PROJECTS, [Roles.COORDINATOR, Roles.ADMIN, Roles.VISITOR]);
+mappings.set(Actions.CREATE_PROJECT, [Roles.COORDINATOR, Roles.ADMIN, Roles.VISITOR]);
+mappings.set(Actions.CREATE_INTERVIEW, [Roles.INTERVIEWER, Roles.COORDINATOR, Roles.ADMIN, Roles.VISITOR]);
+mappings.set(Actions.ASSIGN_INTERVIEWER_ROLE, [Roles.COORDINATOR, Roles.ADMIN, Roles.VISITOR]);
 mappings.set(Actions.UPDATE_PROFILE, [Roles.COORDINATOR, Roles.ADMIN, Roles.VISITOR, Roles.INTERVIEWER]);
 
 function hasPermission(role: string, action: string) {
