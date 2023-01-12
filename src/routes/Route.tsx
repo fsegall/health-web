6b9cabaec48002b1e6c4ca36bc7ea5c5fe3fa2d1
fsegall <segall.felipe@gmail.com>
2023-01-12 00:01:29 -0300

    Revert "Merge pull request #34 from fsegall/feature/ux-improvements"
    This reverts commit 554e6044cbaf1e61ee621d85bcd36c65a47321ac, reversing
    changes made to 9daf768273814915d4e867f4e38ca7ddc56ac2da.

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -4,10 +4,8 @@ import {
   RouteProps as ReactDomRouteProps,
   Redirect,
 } from 'react-router-dom';
-import hasPermission from '../authorization/constants';
 import { useAuth } from '../hooks/auth';
 import AdminTemplate from '../templates/AdminTemplate';
-import { routesOptions } from '../templates/AdminTemplate/components/Menu/menu-routes';
 
 interface RouteProps extends ReactDomRouteProps {
   isPrivate?: boolean;
@@ -22,43 +20,21 @@ const Route: React.FC<RouteProps> = ({
   ...rest
 }) => {
   const { user } = useAuth();
-
-  function handlePagePermissionAccess(isPrivate: boolean) {
-    const route = routesOptions.find(r => r.path === path)
-    if (!isPrivate) {
-      return true
-    }
-    if (isPrivate && route && user?.role) {
-      return hasPermission(user?.role, route?.action)
-    }
-    return false
-  }
-
   return (
     <ReactDomRoute
       {...rest}
       path={path}
       render={({ location }) => {
-        if (!handlePagePermissionAccess(isPrivate)) {
-          return (
-            <Redirect
-            to={{
-              pathname: isPrivate ? '/' : '/profile',
-              state: { from: location },
-            }}
-          />
-          )
-        }
         return isPrivate === !!user ? isPrivate ? (
           <AdminTemplate>
             <Component />
           </AdminTemplate>
-        ) : (
+        ) :  (
           <Component />
         ) : (
           <Redirect
             to={{
-              pathname: isPrivate ? '/' : '/profile',
+              pathname: isPrivate ? '/' : '/dashboard',
               state: { from: location },
             }}
           />
