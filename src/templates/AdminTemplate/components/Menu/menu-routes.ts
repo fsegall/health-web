1c94a6ac8f1d02599e5fb9fadfb169a1cbc5219b
fsegall <segall.felipe@gmail.com>
2023-01-14 20:44:48 -0300

    Resolve conflicts

--- src/templates/AdminTemplate/components/Menu/menu-routes.ts
+++ src/templates/AdminTemplate/components/Menu/menu-routes.ts
@@ -9,44 +9,4 @@ export const getPageName = (path: string) => {
   return routesOptions?.filter(r => r?.path === path)?.[0]?.name
 }
 
-<<<<<<< HEAD
-const routesOptions = [
-  {
-    path: '/dashboard',
-    name: 'Entrevistas',
-    action: Actions.VIEW_MY_INTERVIEWS
-  },
-  {
-    path: '/indigenous-dashboard',
-    name: 'Entrevistas Indígenas',
-    action: Actions.VIEW_MY_INTERVIEWS
-  },
-  {
-    path: '/project',
-    name: 'Novo Projeto',
-    action: Actions.CREATE_PROJECT
-  },
-  {
-    path: '/interviewers',
-    name: 'Pesquisadores',
-    action: Actions.VIEW_ALL_INTERVIEWS
-  },
-  {
-    path: '/accept',
-    name: 'Iniciar Pesquisa',
-    action: Actions.CREATE_INTERVIEW
-  },
-  {
-    path: '/offline',
-    name: 'Registros Offline',
-    action: Actions.VIEW_MY_INTERVIEWS
-  },
-  {
-    path: '/profile',
-    name: 'Perfil',
-    action: Actions.UPDATE_PROFILE
-  },
-]
-=======
 export const routesOptions = routesHandler?.filter(r => r?.displayOnMenu)
->>>>>>> release/v3.1.3
