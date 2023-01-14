8fd5632a984cbbaf5f3ae95683a42c94c793c499
fsegall <segall.felipe@gmail.com>
2023-01-14 20:51:40 -0300

    Fix merge

--- src/pages/Dashboard/Indigenous.tsx
+++ src/pages/Dashboard/Indigenous.tsx
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import hasPermission, { Actions } from '../../authorization/constants';
+import hasPermission, { Actions, Roles } from '../../authorization/constants';
 import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
 import { Container, FilterContainer, IndigenousCard, IndigenousSection, ListTitle, SubHeader } from './styles';
+import Switch from "react-switch";
+
+interface IndigenousBasicInterviewResponse {
+  id: string;
+  municipio: string;
+  aldeia_comunidade: string;
+  tipo_comunidade: string;
+  entrevistador_id: string;
+  projeto_id: string;
+  data_entrevista: Date
+  responsavel_domicilio: string,
+  created_at: Date
+  updated_at: Date
+}
 
 const IndigenousDashboardSection: React.FC = () => {
   const { user, token } = useAuth();
   const [,setIsLoading] = useState(false);
   const [interviews, setInterviews] = useState<any[]>([]);
+  const [showAll, setShowAll] = useState(false)
 
   useEffect(() => {
-
     async function fetchAllInterviews() {
       const interviews = await api.get('/indigenous-interviews', {
         headers: {
@@ -18,12 +32,6 @@ const IndigenousDashboardSection: React.FC = () => {
         },
       });
       setIsLoading(false);
-<<<<<<< HEAD
-      setInterviews(interviews.data);
-    }
-    fetchAllInterviews()
-  }, [token, user.id, user.role]);
-=======
       if (user?.role ===  Roles.INTERVIEWER && !showAll) {
         const filteredInterviews = interviews?.data?.filter((i: IndigenousBasicInterviewResponse) => i?.entrevistador_id === user?.id)
         setInterviews(filteredInterviews);
@@ -33,30 +41,23 @@ const IndigenousDashboardSection: React.FC = () => {
     }
     fetchAllInterviews()
   }, [token, user, showAll, setInterviews]);
->>>>>>> release/v3.1.3
 
   return (
     <>
         <Container>
         <SubHeader>
-          {hasPermission(user.role, Actions.VIEW_ALL_INTERVIEWS) ? <ListTitle>Entrevistas</ListTitle> : <ListTitle>Minhas Entrevistas</ListTitle>}
-
+          {hasPermission(user?.role, Actions?.VIEW_ALL_INTERVIEWS) ? <ListTitle>Entrevistas</ListTitle> : <ListTitle>Minhas Entrevistas</ListTitle>}
           <FilterContainer>
             <h2>Projetos Indígenas</h2>
           </FilterContainer>
 
         </SubHeader>
+        {user?.role === Roles?.INTERVIEWER && (
+          <div style={{ padding: '5px 30px' }}>
+            <Switch onColor="rgb(89,116,140,0.7)" uncheckedIcon={false} checkedIcon={false} offColor="#dedede" onChange={() => setShowAll(!showAll)} checked={showAll} /> <p>{showAll ? 'Todas entrevistas' : 'Minhas entrevistas'}</p>
+          </div>
+        )}
       <IndigenousSection>
-<<<<<<< HEAD
-        {interviews?.map((i: any, index: number) => (
-          <IndigenousCard key={index}>
-            <p><strong>ID:</strong> {i.id}</p>
-            <p><strong>Municipio:</strong> {i.municipio}</p>
-            <p><strong>Aldeia:</strong> {i.aldeia_comunidade}</p>
-            <p><strong>Data:</strong> {new Date(i.created_at).toLocaleDateString('pt-BR')} - {new Date(i.created_at).toLocaleTimeString('pt-BR')}</p>
-          </IndigenousCard>
-        ))}
-=======
         {interviews?.length === 0 ? <p>Nenhuma entrevista cadastrada</p> : (
           interviews?.map((i: IndigenousBasicInterviewResponse, index: number) => (
             <IndigenousCard key={index} isInterviewer={user?.id === i?.entrevistador_id}>
@@ -67,7 +68,6 @@ const IndigenousDashboardSection: React.FC = () => {
             </IndigenousCard>
           ))
         )}
->>>>>>> release/v3.1.3
       </IndigenousSection>
       </Container>
     </>
