7a413c644a159a2a070ea6e6fbb143afb67bd386
Amanda Manduca <amandavmanduca@hotmail.com>
2024-03-23 15:36:44 -0300

    Update domicilio form interview

--- src/pages/Interview/dtos/ICreateHouseholdDTO.ts
+++ src/pages/Interview/dtos/ICreateHouseholdDTO.ts
@@ -3,7 +3,6 @@ export default interface ICreateHouseholdDTO {
   person_id?: string; // Supplied by local storage, not part of user form validation
 
   local_do_domicilio: string;
-  morador_de_rua?: string;
   povos_tradicionais: string;
   qual_povo_tradicional?: string;
   pessoa_de_referencia: string;
